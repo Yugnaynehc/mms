fd0907b55319b1071111de333cf5b87c26e970c4
Yugnaynehc <cyyyyc123@hotmail.com>
2015-03-07 17:12:34 +0800

    add jayq for jQuery wrapper

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/clojurescript "0.0-2850"]
                  [figwheel "0.2.5-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [jayq "2.5.4"]
                  [reagent "0.5.0-alpha3"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
